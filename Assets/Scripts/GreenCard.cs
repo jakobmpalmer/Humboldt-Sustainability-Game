e235d6bff19390d324dd697fb579358533fbc6ac
Jakob Paulson-Palmer <jakobmpalmer@gmail.com>
2020-06-24 18:01:03 -0700

    First commit.

---
+++ Assets/Scripts/GreenCard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GreenCard : MonoBehaviour
+{
+    public int thisSize;
+    private GameObject thisCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCard = this.gameObject;
+        RectTransform rt = thisCard.GetComponent<RectTransform>();//.Set(0,0);
+        rt.sizeDelta = new Vector3(120,120,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
